29c3c07b8b481bc2326edc80cbd366e162eb8513
Volodymyr Prokopiv <prokopiff@gmail.com>
2021-11-26 13:03:15 +0200

    Update push script

--- update-images.sh
+++ update-images.sh
@@ -1,8 +1,3 @@
-docker buildx build . -t vprokopiv/ytbot:arm32v7 --build-arg ARCH=arm32v7 --platform linux/arm/v7
-docker push vprokopiv/ytbot:arm32v7
-
-docker buildx build . -t vprokopiv/ytbot:amd64 --build-arg ARCH=amd64 --platform linux/amd64
-docker push vprokopiv/ytbot:amd64
-
-docker buildx build . -t vprokopiv/ytbot:latest --build-arg ARCH=amd64 --platform linux/amd64
-docker push vprokopiv/ytbot:latest
+docker buildx build . -t vprokopiv/ytbot:arm32v7 --build-arg ARCH=arm32v7 --platform linux/arm/v7 --push
+docker buildx build . -t vprokopiv/ytbot:amd64 --build-arg ARCH=amd64 --platform linux/amd64 --push
+docker buildx build . -t vprokopiv/ytbot:latest --build-arg ARCH=amd64 --platform linux/amd64 --push
