888cc6f29fe34e80e3bd52c90ece43d3f5cbc5d6
Volodymyr Prokopiv <prokopiff@gmail.com>
2022-05-20 15:06:16 +0300

    Add arm64 build

--- update-images.sh
+++ update-images.sh
@@ -1,3 +1,4 @@
 docker buildx build . -t vprokopiv/ytbot:arm32v7 --build-arg ARCH=arm32v7 --platform linux/arm/v7 --push
+docker buildx build . -t vprokopiv/ytbot:arm64 --build-arg ARCH=arm64v8 --platform linux/arm64 --push
 docker buildx build . -t vprokopiv/ytbot:amd64 --build-arg ARCH=amd64 --platform linux/amd64 --push
 docker buildx build . -t vprokopiv/ytbot:latest --build-arg ARCH=amd64 --platform linux/amd64 --push
