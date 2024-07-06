e00594cf477eeb3a8e371ff29dcc97fe63307d9d
Volodymyr Prokopiv <prokopiff@gmail.com>
2024-07-06 18:59:03 +0300

    Fix the script

--- update-images.sh
+++ update-images.sh
@@ -1,11 +1,5 @@
 set -x
 
-suffix="-$1"
-
-if [[ "$1" == "main" ]]; then
-  suffix=""
-fi
-
-docker buildx build . -t vprokopiv/ytbot:arm64${suffix} --build-arg ARCH=arm64v8 --platform linux/arm64 --push
-docker buildx build . -t vprokopiv/ytbot:amd64${suffix} --build-arg ARCH=amd64 --platform linux/amd64 --push
-docker buildx build . -t vprokopiv/ytbot:latest${suffix} --build-arg ARCH=amd64 --platform linux/amd64 --push
+docker buildx build . -t vprokopiv/ytbot:arm64 --build-arg ARCH=arm64v8 --platform linux/arm64 --push
+docker buildx build . -t vprokopiv/ytbot:amd64 --build-arg ARCH=amd64 --platform linux/amd64 --push
+docker buildx build . -t vprokopiv/ytbot:latest --build-arg ARCH=amd64 --platform linux/amd64 --push
