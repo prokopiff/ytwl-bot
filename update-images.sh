2708d3f6f53e4dda4827c3a42f8b6da66404a4ae
Volodymyr Prokopiv <prokopiff@gmail.com>
2024-07-06 18:44:28 +0300

    Build PRs too

--- update-images.sh
+++ update-images.sh
@@ -1,3 +1,5 @@
-docker buildx build . -t vprokopiv/ytbot:arm64 --build-arg ARCH=arm64v8 --platform linux/arm64 --push
-docker buildx build . -t vprokopiv/ytbot:amd64 --build-arg ARCH=amd64 --platform linux/amd64 --push
-docker buildx build . -t vprokopiv/ytbot:latest --build-arg ARCH=amd64 --platform linux/amd64 --push
+set -x
+
+docker buildx build . -t vprokopiv/ytbot:arm64$1 --build-arg ARCH=arm64v8 --platform linux/arm64 --push
+docker buildx build . -t vprokopiv/ytbot:amd64$1 --build-arg ARCH=amd64 --platform linux/amd64 --push
+docker buildx build . -t vprokopiv/ytbot:latest$1 --build-arg ARCH=amd64 --platform linux/amd64 --push
